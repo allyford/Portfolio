433dc34fc76a0d7bf281fffc6ab9c356108797e8
Alex Weininger <alex.weininger@live.com>
2019-09-06 14:52:41 -0700

    popup

--- js/script.js
+++ js/script.js
@@ -4,3 +4,34 @@ child.style.right = child.clientWidth - child.offsetWidth + "px";
 
 let copyrightString = document.getElementById('copyright');
 copyrightString.innerHTML = 'Alex Weininger 2017-' + (new Date).getFullYear();
+
+function togglePopup(element) {
+
+	let popup = document.getElementById('popup-wrapper');
+	console.log(popup);
+	if (popup.style.display !== 'none') {
+		popup.style.display = 'none';
+	} else {
+		popup.style.display = 'block';
+	}
+
+	if (projects.length < 1) {
+		return;
+	}
+
+	let project = null;
+	for (let i = 0; i < projects.length; i++) {
+		if (projects[i].id === element.id) {
+			project = projects[i];
+		}
+	}
+
+	if (project == null) {
+		project = projects[0];
+	}
+
+	document.getElementById("p-title").innerText = project.title;
+	document.getElementById("p-info").innerText = project.longDescription;
+	document.getElementById("p-link").attributes['href'] = project.link;
+	document.getElementById("p-link").innerText = project.linkText;
+}
---
+++ projects.js
@@ -0,0 +1,10 @@
+var projects = [{
+	"id":"sample",
+	"title":"Kataluna Horsemanship",
+	"shortDescription": "short description",
+	"longDescription": "long description long descriptionlong descriptionlong descriptionlong descriptionlong descriptionlong descriptionlong descriptionlong descriptionlong descriptionlong descriptionlong descriptionlong descriptionlong descriptionlong descriptionlong descriptionlong descriptionlong descriptionlong descriptionlong descriptionlong descriptionlong descriptionlong descriptionlong descriptionlong descriptionlong descriptionlong descriptionlong descriptionlong descriptionlong descriptionlong descriptionlong descriptionlong descriptionlong descriptionlong descriptionlong descriptionlong descriptionlong descriptionlong descriptionlong descriptionlong descriptionlong descriptionlong descriptionlong descriptionlong descriptionlong descriptionlong descriptionlong descriptionlong descriptionlong descriptionlong descriptionlong descriptionlong descriptionlong descriptionlong descriptionlong descriptionlong descriptionlong descriptionlong descriptionlong descriptionlong descriptionlong descriptionlong descriptionlong descriptionlong descriptionlong descriptionlong descriptionlong description",
+	"date": "August 2019",
+	"link": "#",
+	"linkText": "View full report as PDF"
+}
+];
